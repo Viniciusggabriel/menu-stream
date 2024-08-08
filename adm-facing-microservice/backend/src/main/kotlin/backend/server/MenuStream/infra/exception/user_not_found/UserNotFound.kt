127bb55ddd612f25dd557164f39a7928cc42f27e
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-08-07 23:10:47 -0300

    chore: Muda o nome da classe que estava no returno do UserNotFoun

--- adm-facing-microservice/backend/src/main/kotlin/backend/server/MenuStream/infra/exception/user_not_found/UserNotFound.kt
+++ adm-facing-microservice/backend/src/main/kotlin/backend/server/MenuStream/infra/exception/user_not_found/UserNotFound.kt
@@ -2,6 +2,6 @@ package backend.server.MenuStream.infra.exception.user_not_found
 
 class UserNotFound(message: String, private val errorCode: Int? = null) : RuntimeException(message) {
     override fun toString(): String {
-        return "ExNotFound(message='${super.message}', errorCode=$errorCode)"
+        return "UserNotFound: (message='${super.message}', errorCode=$errorCode)"
     }
 }
