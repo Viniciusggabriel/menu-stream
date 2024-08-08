6d834e335acfb1ae16795175c5229290761f8327
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-08-08 03:04:34 -0300

    feat: Adiciona a entidade de usuário a nova anotação de validação de username

--- adm-facing-microservice/backend/src/main/kotlin/backend/server/MenuStream/model/entity/user/User.kt
+++ adm-facing-microservice/backend/src/main/kotlin/backend/server/MenuStream/model/entity/user/User.kt
@@ -1,5 +1,6 @@
 package backend.server.MenuStream.model.entity.user
 
+import backend.server.MenuStream.infra.validation.annotation.UsernameValid
 import jakarta.persistence.*
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -18,6 +19,7 @@ class User(
     val idUser: UUID = UUID.randomUUID(),
 
     @Column(name = "DS_USERNAME", unique = true, nullable = false, length = 30)
+    @UsernameValid(message = "O nome de usuário não pode conter caracteres especiais somente ponto '.'")
     private val username: String = "",
 
     @Column(name = "DS_PASSWORD", nullable = false, length = 150)
