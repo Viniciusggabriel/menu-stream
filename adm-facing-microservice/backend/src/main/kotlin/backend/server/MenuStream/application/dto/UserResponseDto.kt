f37de42df83d06ea2065d9754e425c429dc87ffd
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-08-09 12:15:23 -0300

    refactor: Altera o DTO de user response, agora ele retorna um token e um httpstatus

--- adm-facing-microservice/backend/src/main/kotlin/backend/server/MenuStream/application/dto/UserResponseDto.kt
+++ adm-facing-microservice/backend/src/main/kotlin/backend/server/MenuStream/application/dto/UserResponseDto.kt
@@ -1,5 +1,5 @@
 package backend.server.MenuStream.application.dto
 
-import backend.server.MenuStream.model.entity.user.Role
+import org.springframework.http.HttpStatus
 
-data class UserResponseDto(val message: String, val username: String)
+data class UserResponseDto(val token: String, private val errorCode: HttpStatus? = HttpStatus.BAD_REQUEST)
